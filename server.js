a4d965fb4491e138d477ecf8362d2ff0f369fe70
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-08-14 14:21:42 -0500

    added notes file

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const PORT = 3001;
 
 const app = express();
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
