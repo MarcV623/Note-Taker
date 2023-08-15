d937488ed72ef7dc38c469b400f80da82e1f1233
marc vargas <marcvargas@marcs-Air.lan>
2023-08-14 21:17:01 -0500

    intial deploy

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) =>
+  res.json([
+    {
+      id: 1,
+      title: 'My Note!',
+      text: 'Hello World!'
+    },
+    {
+      id: 2,
+      title: 'My Note',
+      text: 'whats up duuude!!!'
+    }
+  ])
+);
+
 app.listen(PORT, () =>
     console.log(`App listening @ http://localhost:${PORT}`)
 );
