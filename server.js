baf3f39761a9e8c7525d05d99baa15c41aa3c820
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-08-16 11:42:53 -0500

    implemented GET /api/notes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,24 +9,15 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.get('/notes', (request, response) => {
+  response.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 
-app.get('/api/notes', (req, res) =>
-  res.json([
-    {
-      id: 1,
-      title: 'My Note!',
-      text: 'Hello World!'
-    },
-    {
-      id: 2,
-      title: 'My Note',
-      text: 'whats up duuude!!!'
-    }
-  ])
-);
+app.get('/api/notes', (request, response) => {
+  let data = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8')
+  data = JSON.parse(data)
+  response.json(data)
+});
 
 app.listen(PORT, () =>
     console.log(`App listening @ http://localhost:${PORT}`)
